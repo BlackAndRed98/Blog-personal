39beb6e3864271fc62b61ff3f9d3eaaaafe51bfa
BlackAndRed98 <fnftrapstudio@gmail.com>
2023-09-01 23:11:21 +0200

    Delete build and Static

--- src/hocs/layouts/Layout.js
+++ src/hocs/layouts/Layout.js
@@ -2,7 +2,6 @@ import { connect } from "react-redux";
 import { motion } from "framer-motion";
 
 function Layout({ children }) {
-  // Usar desestructuración para acceder a "children"
   return (
     <motion.div
       initial={{ opacity: 0, transition: { duration: 0.5 } }}
@@ -14,11 +13,6 @@ function Layout({ children }) {
   );
 }
 
-const mapStateToProps = (state) => {
-  return {
-    // Aquí debes mapear propiedades del estado a propiedades del componente
-    // Por ejemplo: propName: state.someProperty
-  };
-};
+const mapStateToProps = (state) => ({});
 
 export default connect(mapStateToProps, {})(Layout);
